76e589100697fc33afe1db86d232b293c2370d3c
SANTVILAS <santvilaskeshari@gmail.com>
2024-04-14 16:36:33 +0530

    spring data jdbc concept covered

---
+++ spring-data-jdbc/src/main/java/com/javatechie/repository/EmployeeRowMapper.java
@@ -0,0 +1,24 @@
+package com.javatechie.repository;
+
+
+
+import com.javatechie.entity.Employee;
+import org.springframework.jdbc.core.RowMapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+
+public class EmployeeRowMapper implements RowMapper<Employee> {
+
+    @Override
+    public Employee mapRow(ResultSet rs, int rowNum) throws SQLException {
+        return Employee.builder()
+                .id(rs.getInt("id"))
+                .name(rs.getString("name"))
+                .dept(rs.getString("dept"))
+                .email(rs.getString("email"))
+                .doj(rs.getDate("doj"))
+                .build();
+    }
+}
